ec212039610ea0515d09ddeb29302cb2545bc80d
Britt <brittmce@gmail.com>
2026-01-20 14:37:41 -0800

    Initial Commit

---
+++ Video Poker/Video_PokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_PokerApp.swift
+//  Video Poker
+//
+//  Created by Britt McEachern on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Video_PokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Video PokerTests/Video_PokerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Video_PokerTests.swift
+//  Video PokerTests
+//
+//  Created by Britt McEachern on 1/20/26.
+//
+
+import Testing
+@testable import Video_Poker
+
+struct Video_PokerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
